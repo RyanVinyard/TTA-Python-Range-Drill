508209d0aca82643a12423ab53fe84ba64ab31e2
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2018-04-30 12:12:09 -0700

    first and likely only commit

---
+++ range-function.py
@@ -0,0 +1,15 @@
+#   Ryan Vinyard
+#   Python 3.6.5
+#   Short range() drill
+
+print("First drill below\n")
+for i in range(4):
+    print(i)
+
+print("\nSecond drill below\n")
+for i in range(3,-1,-1):
+    print(i)
+
+print("\nThird drill below\n")
+for i in range (8,0,-2):
+    print(i)
